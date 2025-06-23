1b00106921f8b5dfcf6e8e1b9cc930dae9afc75d
CristianX <thecristianx@hotmail.com>
2025-06-22 19:30:15 -0500

    ft - Creando manejo de errores global

---
+++ src/main/java/com/cristianx/backend_facturacion/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.cristianx.backend_facturacion.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllExceptions(Exception ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("error", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Puedes agregar más métodos para excepciones específicas si lo deseas
+}
